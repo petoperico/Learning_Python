292bebcd9da06c3f4ed787445f1993469a563ab1
AntonioDominguez <petoperico@hotmail.com>
2020-10-27 12:41:57 -0600

    Add For script

---
+++ 02for.py
@@ -0,0 +1,22 @@
+"""Class For."""
+"""
+names = ('Abraham', 'César', 'Daniel', 'Daniela', 'Diego', 'Edgar')
+
+for name in names:
+    print(f'Student: {name}')
+else:
+    print('No more names')
+"""
+"""
+string = 'Antonio'
+
+for char in string:
+    if char != 'i':
+        print(char)
+"""
+numbers = []
+
+for number in range(0, 21, 2):
+    numbers.append(number)
+
+print(numbers)
